d6603122d01bc9a228ececd0c0fc7dbfb9db77d7
Madison <madisoncolemit@gmail.com>
2023-07-08 17:48:22 -0400

    chat-app vers 1

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "8f97e74c-c21c-48ed-91fd-be88a6108ae5" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    } else {
+      console.error("Error:", e);
+      return res.status(500).json({ error: "An unexpected error occurred." });
+    }
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Connected!");
+});
